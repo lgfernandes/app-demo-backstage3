d5fad09e060f86b81e00e6b87bb0f9dd8ffac47a
Fernandes <Leandro.Fernandes@iob.com.br>
2025-04-17 15:12:29 -0300

    demo 3

---
+++ src/main/java/com/appbackstage2/app_demo_backstage2/AppDemoBackstage2Application.java
@@ -0,0 +1,13 @@
+package com.appbackstage2.app_demo_backstage2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDemoBackstage2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDemoBackstage2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/appbackstage2/app_demo_backstage2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.appbackstage2.app_demo_backstage2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AppDemoBackstage2Application.class);
+	}
+
+}
